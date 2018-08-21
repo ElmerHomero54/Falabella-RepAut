23f768d70fb1fbd4d9eda78f897ff6152a1cb8f1
Yuri Castro <rcastro@TSE.local>
2018-08-21 18:03:47 -0300

    Correcciones a Mensual

--- crea.bat
+++ crea.bat
@@ -1,12 +1,13 @@
 @echo off
 rem ---
 set EXE=C:\Falabella\AutRep\exe
+PATH=C:\cygwin64\bin;%PATH%
 rem --
 rem -- Calcula odate restando uno a la fecha del sistema
 for /f "delims=" %%a in (' powershell "(Get-Date).AddDays(-1).tostring(\"yyyyMMdd\")" ') do set "odate=%%a"
 echo %odate%
 rem ---
-re, -- Genera cada grafica indicada
+rem -- Genera cada grafica indicada
 %EXE%\creaGrafica.bat "persat" %odate%
 %EXE%\creaGrafica.bat "perfcc" %odate%
 %EXE%\creaGrafica.bat "argsat" %odate%
--- creaDatosGraficaMensual.sh
+++ creaDatosGraficaMensual.sh
@@ -3,21 +3,41 @@
 EXE="/cygdrive/c/Falabella/AutRep/exe"
 DAT="/cygdrive/c/Falabella/AutRep/dat"
 # --
-pais="arg"
-ODATE="20180831"
+pais=$1
+ODATE=$2
 # --
 nomLST="datosDiarios_"$pais".txt"
 # --
-# -- Calcula el rango de fechas para 12 meses (nm)
-rango=$(echo $ODATE |
-         awk -v nm=12 '{ if(nm==12) nm=1
-            finis=$0; yr_f=substr(finis,1,4); mt_f=substr(finis,5,2)
-            yr_i = yr_f - 1; mt_i = mt_f + 1; if(mn_i<0) mn_i=mn_i+nm
-            printf("%04d%02d@%04d%02d\n", yr_i,mt_i,yr_f,mt_f) }')
+# -- Genera archivo temporal mensual
+# --    Borra version anterior
+rm -f $DAT/tmpMensual_$pais.txt
+# --    Calcula y graba el rango de meses
+# --    Genera encabezados de años y meses para la grafica
+echo $ODATE |
+   awk -v nm=12 '
+   BEGIN{ mx="ENERO@FEBRERO@MARZO@ABRIL@MAYO@JUNIO@JULIO@AGOSTO@SEPTIEMBRE@OCTUBRE@NOVIEMBRE@DICIEMBRE"; q=split(mx,meses,"@") }
+   { finis=$0; yr_f=substr(finis,1,4)+0; mt_f=substr(finis,5,2)+0
+     yr=yr_f;mt=mt_f
+     for(i=nm;i>=1;i--) {
+        print sprintf("%04d%02d",yr,mt)"@"meses[mt]
+        mt=mt-1; if(mt<1) { mt=12; yr=yr-1 } } }' |
+   sort -t'@' -nk 1,1 |
+   awk -F'@' '
+   BEGIN{s=FS}
+   NR==1 {inic=$1}
+   { ax=substr($1,1,4); if(ant!=ax) { let=ax; ant=ax } else let=""
+     ano=ano s let
+     mes=mes s $2 }
+   END{finis=$1
+       print inic s finis
+       print substr(ano,2)
+       print substr(mes,2) }' |
+   sed 's/$'"/`echo \\\r`/" > $DAT/tmpMensual_$pais.txt
 # --
 # -- Separa los limites inicial y final del rango calculado
-mi=$(echo $rango | cut -d'@' -f 1)
-mf=$(echo $rango | cut -d'@' -f 2)
+mi=$(head -1 $DAT/tmpMensual_$pais.txt | cut -d'@' -f 1)
+mf=$(head -1 $DAT/tmpMensual_$pais.txt | cut -d'@' -f 2)
+# --
 echo "Rango de fechas para reporte mensual: "$mi "  " $mf
 # --
 # -- Genera archivo temporal con datos del rango de meses
@@ -32,13 +52,12 @@ filtGrp=$(echo $filtGrp | sed 's/.$//')
 # --
 filt=$(echo $filtFin"|"$filtGrp | awk -F'|' '{for(i=1;i<=NF;i++) a[$i]=$i} END{for(i in a) printf("%s|",a[i])}' | awk '{n=length($0);print substr($0,2,n-2)}')
 # --
-# -- Genera archivo temporal mensual
-echo ">>>"$filt"<<<"
+# --    Genera archivo de datos temporal con los datos de los jobs de grupos
 cat $DAT/$nomLST |
    awk -F'@' -v mi=$mi -v mf=$mf '{ anomes=substr($2,1,6); if(anomes>=mi && anomes<=mf) print }' |
    grep -E "$(echo $filt)"  |
    sort |
-   awk -F'@' 'BEGIN{s="@";n=1} @include "../exe/func.txt"
+   awk -F'@' 'BEGIN{s="@";n=1} @include "'$EXE'/func.txt"
    { lei = $1 substr($2,1,6) # -- La clave es el job y el año/mes
      if(cve!=lei) {
         x=length(cve); job=substr(cve,1,x-6);mes=substr(cve,x-5)
@@ -52,9 +71,7 @@ cat $DAT/$nomLST |
    END{printf("%s@%s@%12.8f@%12.8f\n",job,mes,diff_inic/n,diff_finis/n)}' |
    sed '1d' > $DAT/t1_$pais.txt
 # --
-# -- Genera archivo temporal mensual
 # --    Graba los datos de hora de fin de malla
-rm -f $DAT/tmpMensual_$pais.txt
 cat $DAT/t1_$pais.txt |
    grep -E "$(echo $filtFin)" |
    awk -F'@' '{print $2 FS $4}' |
@@ -63,7 +80,7 @@ cat $DAT/t1_$pais.txt |
       NF==1 {print;ant=$1}
       NF>1 {if(ant!=$1) print;ant=$1}
       END{if(ant!=$1) print}' |
-   awk -F'@' '@include "../exe/func.txt"
+   awk -F'@' '@include "'$EXE'/func.txt"
    { print $1 FS strftime("%H%M%S", $2) }' |
    sort -t'@' -nk 1,1 |
    awk -F'@' -v nm=12 -v odate=$ODATE '
@@ -75,7 +92,8 @@ cat $DAT/t1_$pais.txt |
             mt=mt-1;if(mt==0) { mt=12; yr=yr-1 } } }
       { a[$1]=$2 }
       END{for(i in a) printf("%s@",a[i]);printf("\n")}' |
-   sed 's/.$//' > $DAT/tmpMensual_$pais.txt
+   sed 's/.$//' |
+   sed 's/$'"/`echo \\\r`/" >> $DAT/tmpMensual_$pais.txt
 # --
 # -- Procesa los tiempos de proceso de los grupos de aplicacion
 # --
@@ -91,14 +109,8 @@ for dat in $(cat $DAT/grupos.txt | grep '^'$pais'#G@'); do
    grep -E "$fin" $DAT/t1_$pais.txt |
      awk -F'@' '{print $2 FS $4}' |
      sort -unt'@' -k 1,1 > $DAT/t2_mens
-# --
-########echo "******************************************************************"
-########cat $DAT/t1_mens
-########wc -l $DAT/t1_mens
-########cat $DAT/t2_mens
-########wc -l $DAT/t2_mens
-########echo " ------------------------------------------- "
-# --
+   # --
+   # -- Calcula tiempos de ejecucion por grupos de aplicacion
    paste -d'@' $DAT/t1_mens $DAT/t2_mens |
       awk -F'@' 'BEGIN{s=FS}
       { ji[$1]=$2;jf[$3]=$4 }
@@ -111,8 +123,6 @@ for dat in $(cat $DAT/grupos.txt | grep '^'$pais'#G@'); do
                a[ix]=0
                mt=mt-1;if(mt==0) { mt=12; yr=yr-1 } } }
          { a[$1]=$2 }
-         END{for(i in a) printf("%s@",a[i]);printf("\n")}' >> $DAT/tmpMensual_$pais.txt
-   # ---
+         END{for(i in a) printf("%s@",a[i]);printf("\n")}' |
+      sed 's/$'"/`echo \\\r`/" >> $DAT/tmpMensual_$pais.txt
 done
-
-#END{for(i in ji) if(ji[i]!="" && jf[i]!="") print i s strftime("%H%M%S", jf[i] - ji[i]) #jf[i] - ji[i] }'
