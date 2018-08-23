a91d57dc90b5c255daed2c8f61b05fffb2825045
Yuri Castro <rcastro@TSE.local>
2018-08-23 18:47:13 -0300

    Correcciones Grafica Mensual

--- creaDatosGraficaMensual.sh
+++ creaDatosGraficaMensual.sh
@@ -92,7 +92,6 @@ cat $DAT/t1_$pais.txt |
             mt=mt-1;if(mt==0) { mt=12; yr=yr-1 } } }
       { a[$1]=$2 }
       END{for(i in a) printf("%s@",a[i]);printf("\n")}' |
-   sed 's/.$//' |
    sed 's/$'"/`echo \\\r`/" >> $DAT/tmpMensual_$pais.txt
 # --
 # -- Procesa los tiempos de proceso de los grupos de aplicacion
@@ -130,3 +129,53 @@ for dat in $(cat $DAT/grupos.txt | grep '^'$pais'#G@'); do
          END{for(i in a) printf("%s@",a[i]);printf("\n")}' |
       sed 's/$'"/`echo \\\r`/" >> $DAT/tmpMensual_$pais.txt
 done
+# --
+# -- Proceso adicional que busca los datos en archivos mensuales
+# --
+# -- Lee el rango de fechas
+ini=$(head -1 $DAT/tmpMensual_$pais.txt | cut -d'@' -f 1)
+fin=$(head -1 $DAT/tmpMensual_$pais.txt | cut -d'@' -f 2)
+# --
+# -- Obtiene todos los meses del rango
+rango=$(awk -F'@' -v nm=12 'BEGIN{s=FS}
+        NR==1 {ini=$1;fin=$2}
+        NR==2 {ano=substr(ini,1,4)+0;mes=substr(ini,5,2)+0;
+           for(i=1;i<=nm;i ++) { printf("%04d%02d@",ano,mes)
+              mes=mes+1; if(mes>nm) {mes=1;ano=ano+1} }
+           printf("\n"); exit 0 }' $DAT/tmpMensual_$pais.txt | sed 's/.$//g')
+# --
+# -- Prepara la salida para Excel para comparar contra datos anteriores
+cat $DAT/tmpMensual_$pais.txt |
+   sed 's/\(.*\)@/\1 /' |
+   awk -F'@' -v nm=12 -v rango=$rango '
+   BEGIN{s=FS;q=split(rango,cve,"@")}
+   NR>3 { for(i=1;i<=NF;i++) print NR-3 s cve[i] s $i }' > $DAT/tma1
+# --
+# -- Obtiene cuantos datos hay por fecha
+nf=$(head -1 $DAT/dat_men_${pais}_sat.txt | awk -F'@' '{print NF-1}')
+# --
+# -- Busca los datos mensuales anteriores
+#cat $DAT/dat_men_${pais}_sat.txt |
+cat $DAT/dat_men_${pais}_sat.txt |
+   awk -F'@' -v ini=$ini -v fin=$fin '{ if($1>=ini && $1<=fin) print }' |
+   awk -F'@' -v nm=12 -v rango=$rango -v nf=$nf '
+   BEGIN{ s=FS;cr=""; for(i=1;i<=nf;i++) cr=cr"@0"; cr=substr(cr,2)
+      q=split(rango,cve,"@");for(i=1;i<=q;i++) a[cve[i]]=cr }
+   { d="";for(i=2;i<=NF;i++) d=d s $i; d=substr(d,2); a[$1]=d }
+   END{for(i in a) print i s a[i]}' |
+   awk -F'@' '@include "'$EXE'/func.txt"
+      { for(i=2;i<NF;i++) print i-1 FS $1 FS USTime2ExcelTime($i) }' |
+   sort -t'@' -nk1,1 > $DAT/tma2
+# --
+# --
+head -3 $DAT/tmpMensual_$pais.txt > $DAT/tmpMensual_${pais}_2.txt
+paste -d'@' $DAT/tma1 $DAT/tma2 |
+   awk -F'@' '{ cve=$2; monto=$3; if(monto==0 && $6!=0) monto=$6
+                print $1 FS cve FS monto }' |
+   awk -F'@' '{if(ant!=$1) { print substr(sal,2); sal=""; ant=$1 }
+      sal=sal FS $3 }
+   END{print substr(sal,2)}' |
+   sed '1d' >> $DAT/tmpMensual_${pais}_2.txt
+# --
+cp $DAT/tmpMensual_${pais}_2.txt $DAT/tmpMensual_$pais.txt
+rm -f $DAT/tmpMensual_${pais}_2.txt
